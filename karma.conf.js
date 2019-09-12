77983d08e4d435ab205f6a15cfbe5f7fd46ecfa1
Adriano Costa <iano76@gmail.com>
2019-09-12 16:55:15 +0200

    fix(test): use chrome

--- karma.conf.js
+++ karma.conf.js
@@ -1,96 +1,98 @@
 module.exports = function(config) {
-    var configuration = {
-        basePath: '.',
+  var configuration = {
+    basePath: '.',
 
-        frameworks: ['jasmine'],
+    frameworks: ['jasmine'],
 
-        files: [
-            // Polyfills.
-            'node_modules/core-js/client/shim.min.js',
-            // System.js for module loading
-            'node_modules/systemjs/dist/system.src.js',
+    files: [
+      // Polyfills.
+      'node_modules/core-js/client/shim.min.js',
+      // System.js for module loading
+      'node_modules/systemjs/dist/system.src.js',
 
-            // Zone.js dependencies
-            'node_modules/zone.js/dist/zone.js',
-            'node_modules/zone.js/dist/zone-testing.js',
+      // Zone.js dependencies
+      'node_modules/zone.js/dist/zone.js',
+      'node_modules/zone.js/dist/zone-testing.js',
 
-            // RxJs.
-            { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
-            { pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false },
+      // RxJs.
+      { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
+      {
+        pattern: 'node_modules/rxjs/**/*.js.map',
+        included: false,
+        watched: false
+      },
 
+      // paths loaded via module imports
+      // Angular itself
+      {
+        pattern: 'node_modules/@angular/**/*.js',
+        included: false,
+        watched: true
+      },
+      {
+        pattern: 'node_modules/@angular/**/*.js.map',
+        included: false,
+        watched: true
+      },
 
-            // paths loaded via module imports
-            // Angular itself
-            {pattern: 'node_modules/@angular/**/*.js', included: false, watched: true},
-            {pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: true},
+      { pattern: 'karma-test-shim.js', included: true, watched: true },
 
-            {pattern: 'karma-test-shim.js', included: true, watched: true},
+      // Our built application code
+      { pattern: 'compiled/**/*.js', included: false, watched: true },
 
+      // paths loaded via Angular's component compiler
+      // (these paths need to be rewritten, see proxies section)
+      // {pattern: 'dist/**/*.html', included: false, watched: true},
+      // {pattern: 'dist/**/*.css', included: false, watched: true},
 
-            // Our built application code
-            {pattern: 'compiled/**/*.js', included: false, watched: true},
+      // paths to support debugging with source maps in dev tools
+      { pattern: 'src/**/*.ts', included: false, watched: false }
+    ],
 
-            // paths loaded via Angular's component compiler
-            // (these paths need to be rewritten, see proxies section)
-            // {pattern: 'dist/**/*.html', included: false, watched: true},
-            // {pattern: 'dist/**/*.css', included: false, watched: true},
+    // proxied base paths
+    proxies: {
+      // required for component assests fetched by Angular's compiler
+      '/src/': '/base/src/'
+    },
 
-            // paths to support debugging with source maps in dev tools
-            {pattern: 'src/**/*.ts', included: false, watched: false}
-        ],
+    port: 9876,
 
-        // proxied base paths
-        proxies: {
-            // required for component assests fetched by Angular's compiler
-            '/src/': '/base/src/'
-        },
+    logLevel: config.LOG_INFO,
 
-        port: 9876,
+    colors: true,
 
-        logLevel: config.LOG_INFO,
+    autoWatch: true,
 
-        colors: true,
+    browsers: ['Chrome'],
 
-        autoWatch: true,
+    // Karma plugins loaded
+    plugins: [
+      'karma-jasmine',
+      'karma-coverage',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-spec-reporter'
+    ],
 
-        browsers: ['FirefoxHeadless'],
-        customLaunchers: {
-            FirefoxHeadless: {
-                base: 'Firefox',
-                flags: [ '-headless' ],
-            },
-        },
+    // Coverage reporter generates the coverage
+    reporters: [/*'spec', 'progress',*/ 'dots', 'coverage'],
 
-        // Karma plugins loaded
-        plugins: [
-            'karma-jasmine',
-            'karma-coverage',
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-spec-reporter'
-        ],
+    // Source files that you wanna generate coverage for.
+    // Do not include tests or libraries (these files will be instrumented by Istanbul)
+    preprocessors: {
+      'src/**/!(*spec).js': ['coverage']
+    },
 
-        // Coverage reporter generates the coverage
-        reporters: [/*'spec', 'progress',*/ 'dots', 'coverage'],
+    coverageReporter: {
+      reporters: [{ type: 'json', subdir: '.', file: 'coverage-final.json' }]
+    },
 
-        // Source files that you wanna generate coverage for.
-        // Do not include tests or libraries (these files will be instrumented by Istanbul)
-        preprocessors: {
-            'src/**/!(*spec).js': ['coverage']
-        },
+    singleRun: true
+  };
 
-        coverageReporter: {
-            reporters:[
-                {type: 'json', subdir: '.', file: 'coverage-final.json'}
-            ]
-        },
+  if (process.env.TRAVIS) {
+    configuration.reporters = ['spec', 'coverage'];
+  }
 
-        singleRun: true
-    };
-
-    if (process.env.TRAVIS) {
-        configuration.reporters = ['spec', 'coverage'];
-    }
-
-    config.set(configuration);
+  config.set(configuration);
 };
