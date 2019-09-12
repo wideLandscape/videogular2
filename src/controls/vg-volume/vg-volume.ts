ff2ccd3f7ccc0c0c3391307c73312700945d042c
Adriano Costa <iano76@gmail.com>
2019-09-12 16:53:45 +0200

    revert(compatibility): Angular 7

--- src/controls/vg-volume/vg-volume.ts
+++ src/controls/vg-volume/vg-volume.ts
@@ -15,82 +15,91 @@ import { Subscription } from 'rxjs';
   selector: 'vg-volume',
   encapsulation: ViewEncapsulation.None,
   template: `
+    <div
+      #volumeBar
+      class="volumeBar"
+      tabindex="0"
+      role="slider"
+      aria-label="volume level"
+      aria-level="polite"
+      [attr.aria-valuenow]="ariaValue"
+      aria-valuemin="0"
+      aria-valuemax="100"
+      aria-orientation="horizontal"
+      [attr.aria-valuetext]="ariaValue + '%'"
+      (click)="onClick($event)"
+      (mousedown)="onMouseDown($event)"
+    >
+      <div class="volumeBackground" [ngClass]="{ dragging: isDragging }">
         <div
-            #volumeBar
-            class="volumeBar"
-            tabindex="0"
-            role="slider"
-            aria-label="volume level"
-            aria-level="polite"
-            [attr.aria-valuenow]="ariaValue"
-            aria-valuemin="0"
-            aria-valuemax="100"
-            aria-orientation="horizontal"
-            [attr.aria-valuetext]="ariaValue + '%'"
-            (click)="onClick($event)"
-            (mousedown)="onMouseDown($event)">
-            <div class="volumeBackground" [ngClass]="{dragging: isDragging}">
-                <div class="volumeValue" [style.width]="(getVolume() * (100-15)) + '%'"></div>
-                <div class="volumeKnob" [style.left]="(getVolume() * (100-15)) + '%'"></div>
-            </div>
-        </div>
-    `,
+          class="volumeValue"
+          [style.width]="getVolume() * (100 - 15) + '%'"
+        ></div>
+        <div
+          class="volumeKnob"
+          [style.left]="getVolume() * (100 - 15) + '%'"
+        ></div>
+      </div>
+    </div>
+  `,
   styles: [
     `
-        vg-volume {
-            -webkit-touch-callout: none;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-            display: flex;
-            justify-content: center;
-            height: 50px;
-            width: 100px;
-            cursor: pointer;
-            color: white;
-            line-height: 50px;
-        }
-        vg-volume .volumeBar {
-            position: relative;
-            display: flex;
-            flex-grow: 1;
-            align-items: center;
-        }
-        vg-volume .volumeBackground {
-            display: flex;
-            flex-grow: 1;
-            height: 5px;
-            pointer-events: none;
-            background-color: #333;
-        }
-        vg-volume .volumeValue {
-            display: flex;
-            height: 5px;
-            pointer-events: none;
-            background-color: #FFF;
-            transition:all 0.2s ease-out;
-        }
-        vg-volume .volumeKnob {
-            position: absolute;
-            width: 15px; height: 15px;
-            left: 0; top: 50%;
-            transform: translateY(-50%);
-            border-radius: 15px;
-            pointer-events: none;
-            background-color: #FFF;
-            transition:all 0.2s ease-out;
-        }
-        vg-volume .volumeBackground.dragging .volumeValue,
-        vg-volume .volumeBackground.dragging .volumeKnob {
-            transition: none;
-        }
+      vg-volume {
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        display: flex;
+        justify-content: center;
+        height: 50px;
+        width: 100px;
+        cursor: pointer;
+        color: white;
+        line-height: 50px;
+      }
+      vg-volume .volumeBar {
+        position: relative;
+        display: flex;
+        flex-grow: 1;
+        align-items: center;
+      }
+      vg-volume .volumeBackground {
+        display: flex;
+        flex-grow: 1;
+        height: 5px;
+        pointer-events: none;
+        background-color: #333;
+      }
+      vg-volume .volumeValue {
+        display: flex;
+        height: 5px;
+        pointer-events: none;
+        background-color: #fff;
+        transition: all 0.2s ease-out;
+      }
+      vg-volume .volumeKnob {
+        position: absolute;
+        width: 15px;
+        height: 15px;
+        left: 0;
+        top: 50%;
+        transform: translateY(-50%);
+        border-radius: 15px;
+        pointer-events: none;
+        background-color: #fff;
+        transition: all 0.2s ease-out;
+      }
+      vg-volume .volumeBackground.dragging .volumeValue,
+      vg-volume .volumeBackground.dragging .volumeKnob {
+        transition: none;
+      }
     `
   ]
 })
 export class VgVolume implements OnInit, OnDestroy {
   @Input() vgFor: string;
-  @ViewChild('volumeBar', { static: true })
+  @ViewChild('volumeBar')
   volumeBarRef: ElementRef;
 
   elem: HTMLElement;
@@ -130,14 +139,14 @@ export class VgVolume implements OnInit, OnDestroy {
     this.isDragging = true;
   }
 
-  @HostListener('document:mousemove', [ '$event' ])
+  @HostListener('document:mousemove', ['$event'])
   onDrag(event: { clientX: number }) {
     if (this.isDragging) {
       this.setVolume(this.calculateVolume(event.clientX));
     }
   }
 
-  @HostListener('document:mouseup', [ '$event' ])
+  @HostListener('document:mouseup', ['$event'])
   onStopDrag(event: { clientX: number }) {
     if (this.isDragging) {
       this.isDragging = false;
@@ -147,7 +156,7 @@ export class VgVolume implements OnInit, OnDestroy {
     }
   }
 
-  @HostListener('keydown', [ '$event' ])
+  @HostListener('keydown', ['$event'])
   arrowAdjustVolume(event: KeyboardEvent) {
     if (event.keyCode === 38 || event.keyCode === 39) {
       event.preventDefault();
@@ -162,7 +171,7 @@ export class VgVolume implements OnInit, OnDestroy {
     const recObj = this.volumeBarRef.nativeElement.getBoundingClientRect();
     const volumeBarOffsetLeft: number = recObj.left;
     const volumeBarWidth: number = recObj.width;
-    return (mousePosX - volumeBarOffsetLeft) / volumeBarWidth * 100;
+    return ((mousePosX - volumeBarOffsetLeft) / volumeBarWidth) * 100;
   }
 
   setVolume(vol: number) {
@@ -175,6 +184,6 @@ export class VgVolume implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.subscriptions.forEach((s) => s.unsubscribe());
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 }
